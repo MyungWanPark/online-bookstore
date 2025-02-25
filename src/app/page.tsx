7b270ee9086712bc399e533e43a763da557c261d
MyungWan Park <mywanpark@gmail.com>
2025-02-25 19:48:28 +0900

    feat: fetching total books api for pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import Image from "next/image";
 import BookCards from "./components/BookCards";
 
-export default function Home() {
+async function fetchBooks(page: number) {
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/books?page=${page}`,
+        { cache: "no-store" }
+    );
+    if (!res.ok) {
+        throw new Error("Failed to fetch books");
+    }
+
+    return res.json();
+}
+
+export default async function Home() {
+    const { books, totalPages } = await fetchBooks(1);
+
     return (
         <div>
             <BookCards />
