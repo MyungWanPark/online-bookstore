ff7f27f1927e3abc54cdaa557428839f414431ef
MyungWan Park <mywanpark@gmail.com>
2025-02-25 23:36:24 +0900

    feat: set keyword for updating data in home  when typing in search input

--- src/app/components/SearchInput.tsx
+++ src/app/components/SearchInput.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import { useState } from "react";
+import useSWR from "swr";
 
 type Props = {
     setKeyword: (k: string) => void;
 };
 
 export default function SearchInput({ setKeyword }: Props) {
-    return <div>검색창</div>;
+    const [input, setInput] = useState("");
+    const { data: realTimeData } = useSWR(`/api/search?q=${input}`);
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+    };
+
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault();
+        setKeyword(input);
+    };
+
+    return (
+        <form onSubmit={handleSearch}>
+            <input
+                type="text"
+                value={input}
+                onChange={handleInput}
+                placeholder="please type here..."
+            />
+            <button type="submit">🔍</button>
+        </form>
+    );
 }
