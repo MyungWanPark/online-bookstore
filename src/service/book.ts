2df6a0fb5922b022bf821f88cceb337c000e9315
MyungWan Park <mywanpark@gmail.com>
2025-02-25 22:44:48 +0900

    feat: change dummy data to test search input

--- src/service/book.ts
+++ src/service/book.ts
@@ -1,9 +1,40 @@
 import { faker } from "@faker-js/faker";
 
+// 검색창 테스트용 책 제목
+const commonTitles = [
+    "Shadow",
+    "Light",
+    "Dream",
+    "Storm",
+    "Echo",
+    "Whisper",
+    "Fire",
+    "Secret",
+    "Legend",
+    "Destiny",
+];
+
+// 검색창 테스트용 저자 이름
+const commonAuthors = [
+    "Smith",
+    "Johnson",
+    "Williams",
+    "Brown",
+    "Jones",
+    "Garcia",
+    "Miller",
+    "Davis",
+    "Rodriguez",
+    "Martinez",
+];
+
+const getRandomElement = (arr: string[]) =>
+    arr[Math.floor(Math.random() * arr.length)];
+
 const BOOKS = Array.from({ length: 105 }, (_, i) => ({
     id: i + 1,
-    title: faker.book.title(),
-    author: faker.book.author(),
+    title: `${getRandomElement(commonTitles)} faker.book.title()`,
+    author: `${getRandomElement(commonAuthors)} faker.book.author()`,
     genre: faker.book.genre(),
     content: faker.lorem.words(30),
     imageUrl: `https://picsum.photos/seed/${i + 1}/300/200`,
